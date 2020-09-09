b92ab264c42b027d6420876b11552532385bf4aa
truong manh chung <truongmanhchung207@gmail.com>
2020-09-09 10:23:34 +0700

    add date

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args ){
+        Date now=new Date();
+        System.out.println("Now is: "+now);
+    }
+}
